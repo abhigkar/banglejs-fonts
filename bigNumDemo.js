a3f895b4f46860bf1fc73e3512d5cb5f2a488860
abhinavgolwalkar <abhinav.golwalkar@gmail.com>
2019-12-05 22:20:52 +0000

    Create bigNumDemo.js

---
+++ bigNumDemo.js
@@ -0,0 +1,18 @@
+g.clear();
+
+require("https://github.com/abhinavgolwalkar/banglejs-fonts/blob/master/num40x58.js").add(Graphics);
+
+g.setNum40x58();
+
+g.setColor(1,0,1);
+let num = 110;
+function tt(){
+  g.clear();
+  g.drawString(num,(g.getWidth()/2)-20,(g.getHeight()/2)-29);
+  num++;
+}
+setInterval(tt,1000);
+
+
+
+
